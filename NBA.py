4823786df1712667756828f31eff6bb8cf4f4418
Ameen Abdur-Rasheed <ameenabdur-rasheed@Ameens-MacBook-Air.local>
2025-04-09 17:01:44 -0400

    Work progress

--- NBA.py
+++ NBA.py
@@ -1,40 +1,65 @@
 import requests
+import json
 # from balldontlie import BalldontlieAPI
-
 
 
 def get_api_key(filename):
     with open(filename, 'r') as file:
-        print(file)
         api_data = file.read().strip()
         # api_key = api_data.split("=")[1].strip().strip('"') 
-        print(api_data)
         return api_data 
-    
 
 
-# def get_teams():
-#     url = "https://api.balldontlie.io/v1/teams"
-#     response = requests.get(url)
-    
-#     if response.status_code == 200:
-#         teams = response.json()["data"]
-#         for team in teams:
-#             print(f"ID: {team['id']}, Team: {team['full_name']}")
-#     else:
-#         print(f"Error: {response.status_code}")
+# url = "https://api-nba-v1.p.rapidapi.com/players/statistics"
 
-# # Call the function
-# get_teams()
+# querystring = {"game":"8133"}
 
-# def get_players(player_id=None):
-#     url = "https://api.balldontlie.io/v1/players"
-#     response = requests.get(url)
-    
-#     if response.status_code == 200:
-#         players = response.json()["data"]
-#         for player in players:
-#             print(f"ID: {player['id']}, Player: {player['first_name']} {player['last_name']}")
-#     else:
-#         print(f"Error: {response.status_code}")
+# headers = {
+# 	"x-rapidapi-key": "71f6ebba9amsh87b761e3e6eddd5p1ac7eajsnec34507127ea",
+# 	"x-rapidapi-host": "api-nba-v1.p.rapidapi.com"
+# }
 
+# response = requests.get(url, headers=headers, params=querystring)
+#print(response.text)
+def get_season():
+    url = "https://api-nba-v1.p.rapidapi.com/seasons"
+
+    headers = {
+        "x-rapidapi-key": "71f6ebba9amsh87b761e3e6eddd5p1ac7eajsnec34507127ea",
+        "x-rapidapi-host": "api-nba-v1.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    print(response.json())
+    #print(f"Status Code: {response.status_code}")
+
+
+def get_teams():
+    url = "https://api-nba-v1.p.rapidapi.com/teams"
+
+    querystring = {"id":"1"}
+
+    headers = {
+        "x-rapidapi-key": "71f6ebba9amsh87b761e3e6eddd5p1ac7eajsnec34507127ea",
+        "x-rapidapi-host": "api-nba-v1.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    print(response.json())
+
+
+
+def get_statistics():
+    url = "https://api-nba-v1.p.rapidapi.com/players/statistics"
+
+    querystring = {"game":"8133"}
+
+    headers = {
+        "x-rapidapi-key": "71f6ebba9amsh87b761e3e6eddd5p1ac7eajsnec34507127ea",
+        "x-rapidapi-host": "api-nba-v1.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+    print(response.json())
